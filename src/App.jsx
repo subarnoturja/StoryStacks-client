5f39d23a84fbdb37a07dd955de6e45e21d1040f9
Subarno Ranjan Barua <subarnoturja15@gmail.com>
2024-12-06 19:39:09 +0600

    setup done

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl text-red-600 text-center">Vite + React</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+
+const Home = () => {
+    return (
+        <div>
+           <p className="text-red-400 text-center text-3xl">This is home</p>
+        </div>
+    );
+};
+
+export default Home;
