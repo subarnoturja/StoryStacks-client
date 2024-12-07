053c1c4d4953e392c79174d857fb0b23108ee060
Subarno Ranjan Barua <subarnoturja15@gmail.com>
2024-12-07 16:41:38 +0600

    Favorite book section added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,14 @@
 import Banner from "../components/Banner";
 import BestSellerBooks from "../components/BestSellerBooks";
+import FavouriteBook from "../components/FavoriteBook";
+
 
 const Home = () => {
     return (
         <div>
            <Banner></Banner>
            <BestSellerBooks />
+           <FavouriteBook></FavouriteBook>
         </div>
     );
 };
