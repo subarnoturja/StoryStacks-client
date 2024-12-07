0552b8d8e30381a6e2205b7f0076c034f868dd3e
Subarno Ranjan Barua <subarnoturja15@gmail.com>
2024-12-07 22:26:04 +0600

    form submission added

--- src/dashboard/SideBar.jsx
+++ src/dashboard/SideBar.jsx
@@ -1,9 +1,14 @@
-import { MdOutlineDriveFolderUpload, MdOutlineLogout, MdOutlineManageHistory } from "react-icons/md";
-import profilePicture from "../assets/profile.jpg"
+import {
+  MdOutlineDriveFolderUpload,
+  MdOutlineLogout,
+  MdOutlineManageHistory,
+} from "react-icons/md";
+import profilePicture from "../assets/profile.jpg";
 import { LuLayoutDashboard } from "react-icons/lu";
 import { FiUserCheck } from "react-icons/fi";
 import { AiOutlineProduct } from "react-icons/ai";
 import { IoSettingsOutline } from "react-icons/io5";
+import { Link } from "react-router-dom";
 
 const Sidebar = () => {
   return (
@@ -17,45 +22,44 @@ const Sidebar = () => {
         <div>
           <h2 className="text-lg font-semibold">Leroy Jenkins</h2>
           <span className="flex items-center space-x-1">
-            <p
-              className="text-xs hover:underline dark:text-gray-600"
-            >
-              Admin
-            </p>
+            <p className="text-xs hover:underline dark:text-gray-600">Admin</p>
           </span>
         </div>
       </div>
       <div className="divide-y dark:divide-gray-300">
         <ul className="pt-2 pb-4 space-y-1 text-sm">
-          <li className="dark:bg-gray-100 dark:text-gray-900">
-            <a
-              rel="noopener noreferrer"
-              href="/admin/dashboard"
-              className="flex items-center p-2 space-x-3 rounded-md"
-            >
-              <LuLayoutDashboard size={24} />
-              <span>Dashboard</span>
-            </a>
+          <li>
+            <Link to="/admin/dashboard">
+              <a
+                rel="noopener noreferrer"
+                className="flex items-center p-2 space-x-3 rounded-md"
+              >
+                <LuLayoutDashboard size={24} />
+                <span>Dashboard</span>
+              </a>
+            </Link>
           </li>
           <li>
+            <Link to='/admin/dashboard/upload'>
             <a
               rel="noopener noreferrer"
-              href="/admin/dashboard/upload"
               className="flex items-center p-2 space-x-3 rounded-md"
             >
               <MdOutlineDriveFolderUpload size={24} />
               <span>Upload Books</span>
             </a>
+            </Link>
           </li>
           <li>
+            <Link to="/admin/dashboard/manage">
             <a
               rel="noopener noreferrer"
-              href="/admin/dashboard/manage"
               className="flex items-center p-2 space-x-3 rounded-md"
             >
               <MdOutlineManageHistory size={24} />
               <span>Manage Books</span>
             </a>
+            </Link>
           </li>
           <li>
             <a
--- src/dashboard/UploadBook.jsx
+++ src/dashboard/UploadBook.jsx
@@ -1,11 +1,103 @@
+import { useState } from "react";
 
 
 const UploadBook = () => {
-    return (
-        <div>
-           UploadBook 
+
+    const bookCategories = [
+        "Fiction",
+        "Non-Fiction",
+        "Mistery",
+        "Programming",
+        "Science Fiction",
+        "Fantasy",
+        "Horror",
+        "Bibliography",
+        "Autobiography",
+        "History",
+        "Self-help",
+        "Memoir",
+        "Business",
+        "Children Books",
+        "Travel",
+        "Religion",
+        "Art and Design"
+    ]
+
+    const [selectedBookCategory, setSelectedBookCategory] = useState(bookCategories[0]);
+
+    // Handle Category 
+    const handleChangeSelectedValue = (event) => {
+        setSelectedBookCategory(event.target.value);
+    }
+
+    // Handle Book Submission
+    const handleBookSubmit = (event) => {
+        event.preventDefault();
+        const form = event.target;
+        const bookTitle = form.bookTitle.value;
+        const authorName = form.authorName.value;
+        const imageURL = form.imageURL.value;
+        const category = form.categoryName.value;
+        const bookDescription = form.bookDescription.value;
+        const bookPDFURL = form.bookPDFURL.value;
+        const bookObject = {
+            bookTitle, authorName, imageURL, category, bookDescription, bookPDFURL
+        }
+        console.log(bookObject);
+    }
+
+  return (
+    <div className="px-4 my-12">
+      <h2 className="mb-8 text-3xl font-bold">Upload Book</h2>
+
+      <form onSubmit={handleBookSubmit} className="flex lg:w-[1180px] flex-col flex-wrap gap-4">
+      <div className="grid sm:grid-cols-2 gap-10">
+        <div className="relative flex items-center">
+          <label className="text-[13px] bg-white text-black absolute px-2 top-[-10px] left-[18px]">Book Title</label>
+          <input type="text" placeholder="Enter Book Title" name="bookTitle"
+            className="px-4 py-3.5 bg-white text-black w-full text-sm border-2 border-gray-100 focus:border-blue-500 rounded outline-none" />
         </div>
-    );
+
+        <div className="relative flex items-center">
+          <label className="text-[13px] bg-white text-black absolute px-2 top-[-10px] left-[18px]">Author Name</label>
+          <input type="text" placeholder="Enter Author Name" name="authorName"
+            className="px-4 py-3.5 bg-white text-black w-full text-sm border-2 border-gray-100 focus:border-blue-500 rounded outline-none" />
+        </div>
+
+        <div className="relative flex items-center">
+          <label className="text-[13px] bg-white text-black absolute px-2 top-[-10px] left-[18px]">Book Image</label>
+          <input type="text" placeholder="Book Image URL" name="imageURL"
+            className="px-4 py-3.5 bg-white text-black w-full text-sm border-2 border-gray-100 focus:border-blue-500 rounded outline-none" />
+        </div>
+
+        <div className="relative flex items-center">
+          <label className="text-[13px] bg-white text-black absolute px-2 top-[-10px] left-[18px]">Category</label>
+          <select name="categoryName" id="" className="px-4 py-3.5 bg-white text-black w-full text-sm border-2 border-gray-100 focus:border-blue-500 rounded outline-none" onChange={handleChangeSelectedValue}>
+                {
+                    bookCategories.map((option) => <option key={option} value={option}>{option}</option>)
+                }
+          </select>
+        </div>
+
+        <div className="relative flex items-center sm:col-span-2">
+          <label className="text-[13px] bg-white text-black absolute px-2 top-[-10px] left-[18px]">Book Description</label>
+          <textarea type="text" placeholder="Write your Book Description" name="bookDescription"
+            className="px-4 py-3.5 bg-white text-black w-full text-sm border-2 border-gray-100 focus:border-blue-500 rounded outline-none" />
+        </div>
+
+        <div className="relative flex items-center sm:col-span-2">
+          <label
+            className="text-[13px] bg-white text-black absolute px-2 top-[-10px] left-[18px]">Book PDF URL</label>
+          <input type="text" placeholder="Book pdf url" name='bookPDFURL'
+            className="px-4 py-3.5 bg-white text-black w-full text-sm border-2 border-gray-100 focus:border-blue-500 rounded outline-none" />
+        </div>
+      </div>
+
+      <button type="submit"
+        className="btn mt-8 px-6 py-2.5 w-full text-sm bg-blue-500/80 text-white rounded hover:bg-blue-600 transition-all">Submit</button>
+    </form>
+    </div>
+  );
 };
 
 export default UploadBook;
