0b119124c5941e13939e0eae8083f7009b81352b
Subarno Ranjan Barua <subarnoturja15@gmail.com>
2024-12-07 19:21:40 +0600

    slide changes are made

--- src/dashboard/DashboardLayout.jsx
+++ src/dashboard/DashboardLayout.jsx
@@ -1,9 +1,10 @@
+import { Outlet } from "react-router-dom";
 
 
 const DashboardLayout = () => {
     return (
         <div>
-            
+            <Outlet></Outlet>
         </div>
     );
 };
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -6,6 +6,8 @@ import Shop from "../pages/Shop";
 import DashboardLayout from "../dashboard/DashboardLayout";
 import Dashboard from "../dashboard/Dashboard";
 import UploadBook from "../dashboard/UploadBook";
+import ManageBooks from "../dashboard/ManageBooks";
+import EditBooks from "../dashboard/EditBooks";
 
 export const router = createBrowserRouter([
     {
@@ -38,7 +40,16 @@ export const router = createBrowserRouter([
             {
                 path: '/admin/dashboard/upload',
                 element: <UploadBook></UploadBook>,
-            }
+            },
+            {
+                path: '/admin/dashboard/manage',
+                element: <ManageBooks></ManageBooks>,
+            },
+            {
+                path: '/admin/dashboard/edit-books/:id',
+                element: <EditBooks></EditBooks>,
+                loader: ({ params }) => fetch(`http://localhost:5000/book/${params.id}`)
+            },
         ]
     }
 ])
